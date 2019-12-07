a6e450056c46d853cf4ad133dd249930725e2500
matiasandina <matiasandina@gmail.com>
2019-12-07 09:55:02 -0500

    fix recommend_library to avoid calling install.packages()

--- R/recommend_libraries.R
+++ R/recommend_libraries.R
@@ -1,18 +1,34 @@
 # TODO ask user to install stuff if non of the requirements are met
 # change this to something like .onAttach
 
-recommend_library <- function(lib_name){
-  # Calling installed.packages() many times is slow
-  if (!exists(".installed", envir = .GlobalEnv)) {
-    .installed <- installed.packages()
-    # remember for later
-    assign('.installed', .installed, envir = .GlobalEnv)
+
+.onAttach <- function(...){
+
+  .packages <- c('rstudioapi', 'tcltk', 'rChoiceDialogs', 'gWidgets2RGtk2')
+  .installed <- sapply(.packages, function(x) requireNamespace(x, quietly = TRUE))
+   # remember for later
+   assign('.installed', .installed, envir = .GlobalEnv)
+
+  recommend_library(.installed, .packages)
+}
+
+recommend_library <- function(.installed, .packages){
+
+  # check windows
+  win_os <- exists('utils::choose.dir')
+
+  can_use <- any(win_os, .installed)
+
+  if (!can_use) {
+    # suggest install
+    message("You don't have any of the recommended packages installed.")
+
+    to_install <- numeric_menu(names(.installed),
+                               prompt = "Which package do you want to install?")
+
+    install.packages(to_install, dependencies = TRUE)
+    require(lib_name, quietly = TRUE, character.only = TRUE)
+
   }
 
-  x <- lib_name %in% .installed
-  #if (!x) {
-  #  install.packages(lib_name, dependencies = TRUE, quiet = TRUE)
-  #  x = require(lib_name, quietly = TRUE, character.only = TRUE)
-  #}
-  return(x)
 }
